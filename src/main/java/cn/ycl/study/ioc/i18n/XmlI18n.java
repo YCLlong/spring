57e03bc684a7623059a22be5b6adc9a5e1b47f93
YCLlong <251121753@qq.com>
2019-10-09 16:41:18 +0800

    国际化

---
+++ src/main/java/cn/ycl/study/ioc/i18n/XmlI18n.java
@@ -0,0 +1,21 @@
+package cn.ycl.study.ioc.i18n;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Locale;
+
+public class XmlI18n {
+    public static void main(String[] args) {
+        MessageSource messageSource = getMessageSource();
+        String msgUs = messageSource.getMessage("tip",new Object[]{"name","code"}, Locale.US);
+        System.out.println(msgUs);
+        String msgChina = messageSource.getMessage("tip",new Object[]{"姓名","编号"}, Locale.CHINA);
+        System.out.println(msgChina);
+    }
+
+    public static MessageSource getMessageSource(){
+        MessageSource source = new ClassPathXmlApplicationContext("classpath:sping-context.xml");
+        return source;
+    }
+}
